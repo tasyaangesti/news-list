f35783ca9566e996a68f29dee2b49145488ca92e
tasyaangesti <tasyahestiaangesti@gmail.com>
2024-08-02 19:26:10 +0700

    set up client

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -1,9 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Login } from "./views/Login";
+import { Home } from "./views/Home";
+import { Layout } from "./views/Layout";
 
 export const router = createBrowserRouter([
   {
     path: "/login",
     element: <Login />,
   },
+  { element: <Layout />, children: [{ path: "/", element: <Home /> }] },
 ]);
