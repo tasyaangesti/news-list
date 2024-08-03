5bd83b8293a2f8df2fc42495e9a085034a3542fc
tasyaangesti <tasyahestiaangesti@gmail.com>
2024-08-03 21:01:57 +0700

    finish

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const UserController = require("./controller/userController");
 const Controller = require("./controller/controller");
 const app = express();
-const port = 3000;
+const port = process.env.SERVER_PORT;
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
